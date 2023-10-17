6190d4abc5b4f89165f16c334d39fb0723e7f16c
arcpac <antonraphaelcaballes@gmail.com>
2023-10-17 16:44:41 +1100

    update test code

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,15 +2,13 @@ const request = require("supertest")
 const app = require("../../app")
 describe("Test GET /launches API", () => {
     test("Display all launches status 200", async () => {
-        const response = await request(app).get("/launches");
-        expect(response.status).toBe(200);
+        const response = await request(app).get("/launches").expect(200);
     })
 })
 
-describe("Test POST /launche", () => {
-    test("It should 200 success", () => {
-        const response = 200;
-        expect(response).toBe(200);
+describe("Test POST /launch", () => {
+    test("create launch status 200", async () => {
+
     })
     test("It should catch invalid dates", () => {
         const response = 400;
